d273925ebfa157867fa2a0a5a53cad8d5a6a46dc
Markus Sanders <markus.sanderst@gmail.com>
2019-05-07 21:14:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  output = {}
+  data.each { |attribute, qualities|
+    qualities.each { |quality, birds|
+      birds.each { |bird|
+        if output[bird] == nil
+          output[bird] = {}
+        end
+        if output[bird][attribute]
+          output[bird][attribute] << quality.to_s
+        else
+          output[bird][attribute] = [quality.to_s]
+        end
+
+      }
+    }
+  }
+  output
 end
