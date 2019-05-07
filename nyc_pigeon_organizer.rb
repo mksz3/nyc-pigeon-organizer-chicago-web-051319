8a51480bfeb7c9cccb04f5b8bc1d768c675e2bf2
Markus Sanders <markus.sanderst@gmail.com>
2019-05-07 21:14:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,14 +3,13 @@ def nyc_pigeon_organizer(data)
   data.each { |attribute, qualities|
     qualities.each { |quality, birds|
       birds.each { |bird|
-        # output[bird] == nil ? output[bird] = {} : output[bird][attribute] ? output[bird][attribute] << quality.to_s : output[bird][attribute] = [quality.to_s] 
-        if output[bird] == nil #if bird doesbt already exist in object
+        if output[bird] == nil
           output[bird] = {}
         end
-        if output[bird][attribute] #if bird's color/gender/home exists
-          output[bird][attribute] << quality.to_s # add birds "" to array
+        if output[bird][attribute]
+          output[bird][attribute] << quality.to_s
         else
-          output[bird][attribute] = [quality.to_s] # create array to hold qualities
+          output[bird][attribute] = [quality.to_s]
         end
 
       }
@@ -18,59 +17,3 @@ def nyc_pigeon_organizer(data)
   }
   output
 end
-
-# pigeon_data = {
-#   :color => {
-#     :purple => ["Theo", "Peter Jr.", "Lucky"],
-#     :grey => ["Theo", "Peter Jr.", "Ms. K"],
-#     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-#     :brown => ["Queenie", "Alex"]
-#   },
-#   :gender => {
-#     :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-#     :female => ["Queenie", "Ms. K"]
-#   },
-#   :lives => {
-#     "Subway" => ["Theo", "Queenie"],
-#     "Central Park" => ["Alex", "Ms. K", "Lucky"],
-#     "Library" => ["Peter Jr."],
-#     "City Hall" => ["Andrew"]
-#   }
-# }
-# pigeon_list = {
-#   "Theo" => {
-#     :color => ["purple", "grey"],
-#     :gender => ["male"],
-#     :lives => ["Subway"]
-#   },
-#   "Peter Jr." => {
-#     :color => ["purple", "grey"],
-#     :gender => ["male"],
-#     :lives => ["Library"]
-#   },
-#   "Lucky" => {
-#     :color => ["purple"],
-#     :gender => ["male"],
-#     :lives => ["Central Park"]
-#   },
-#   "Ms. K" => {
-#     :color => ["grey", "white"],
-#     :gender => ["female"],
-#     :lives => ["Central Park"]
-#   },
-#   "Queenie" => {
-#     :color => ["white", "brown"],
-#     :gender => ["female"],
-#     :lives => ["Subway"]
-#   },
-#   "Andrew" => {
-#     :color => ["white"],
-#     :gender => ["male"],
-#     :lives => ["City Hall"]
-#   },
-#   "Alex" => {
-#     :color => ["white", "brown"],
-#     :gender => ["male"],
-#     :lives => ["Central Park"]
-#   }
-# }
